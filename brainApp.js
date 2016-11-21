b907b344848439ad5db7534e1fc132353b956c0b
danielurencio <danielurencio@gmail.com>
2016-11-20 18:22:05 -0600

    a

--- brainApp.js
+++ brainApp.js
@@ -1,8 +1,14 @@
+var express = require('express');
+var app = express();
+var http = require("http");
+var httpServer = http.createServer(app);
+var clientSocket = require("socket.io-client");
+var io = require('socket.io')(httpServer);
 var net = require("net");
-var io = require('socket.io-client');
+var ioC = require('socket.io-client');
 var host1 = "http://localhost:8080";
 var host2 = "http://iot-urencio.rhcloud.com:8000"
-var clientSocket = io.connect(host2, {reconnect: true});
+var clientSocket = ioC.connect(host2, {reconnect: true});
 
 var HOST = "192.168.0.6";
 var PORT = 5000;
@@ -12,7 +18,8 @@ var server = net.createServer({ 'encoding':'utf8'}, function(socket) {
 
   socket.on("data", function (data) {
     console.log(data);
-    clientSocket.emit("cholula",data);
+    io.emit("ya",data);
+//    clientSocket.emit("cholula",data);
   });
 
 });
@@ -20,3 +27,13 @@ var server = net.createServer({ 'encoding':'utf8'}, function(socket) {
 server.listen(PORT,HOST);
 
 console.log("Server listening on " + HOST + ":" + PORT);
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(req, res) {
+        res.sendFile(__dirname + '/temperatura.html');
+});
+
+httpServer.listen(3000, '0.0.0.0', function() {
+        console.log('El servidor está listo...');
+});
